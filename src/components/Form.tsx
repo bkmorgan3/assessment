8daabb7656cb1851e3aa2bc2cad1c7dc643c8260
Brad Morgan <bkmorgan3@gmail.com>
2024-06-28 13:56:04 -0700

    remove dupe header

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -36,26 +36,6 @@ export default function Form({
   }
   return (
     <>
-      <header className="bg-white px-3.5">
-        <nav className="py-6">
-          <ul className="flex justify-between">
-            <li>
-              <h1 className="text-3xl">Form Title</h1>
-            </li>
-            <li>
-              <Button
-                onClick={() => setName('')}
-                size="3"
-                className="text-indigo-700 bg-slate-100 border-transparent cursor-pointer"
-                radius="full"
-              >
-                Cancel
-              </Button>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
       <div
         style={{ border: '1px solid green' }}
         className="h-full max-w-[880px] min-w-[560px] self-center"
